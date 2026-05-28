2b4cb24f3108735999e4de7e6a50e8efdcc2f2d5
jaspreet <jasspreet4640@gmail.com>
2026-05-28 10:45:58 -0500

    add css to page not found

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -2,11 +2,15 @@ import React from 'react';
 
 export default function NotFound() {
   return (
-    <main>
+      <main className="not-found-page">
+        <div className="not-found-container">
       <h1>404</h1>
       <h2>Page Not Found</h2>
-      <p>The destination link requested does not exist.</p>
+      <p>
+        The destination link requested does not exist.
+      </p>
       <a href="/" className="shop-btn">Return to Home</a>
+      </div>
     </main>
   );
 }
